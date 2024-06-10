6699502b11573343de136c56beffdb43b7347f88
Smooozy01 <142884444+Smooozy01@users.noreply.github.com>
2024-06-10 11:52:09 +0500

    Upload #2

--- Card.java
+++ Card.java
@@ -13,17 +13,37 @@ public class Card {
     // Constructor
     private final String value;
     private final Suit suit;
+    boolean trump;
 
     public Card(String value, Suit suit){
-        this.value = value; this.suit = suit;
+        this.value = value; this.suit = suit; this.trump = false;
     }
 
+
     // Getters
-    public String getCard(){ return (suit + " " + value); }
+    public String getCard(){
+        String isTrump = trump ? " + Trump" : "";
+        return suit + " " + value + isTrump;
+    }
+
 
     public Suit getSuit() { return suit; }
 
-    public String getValue(){ return value; }
+
+    public String getValueString(){ return value; }
+
+
+    public int getValueInt(){
+
+        return switch (value) {
+            case "J" -> 11;
+            case "Q" -> 12;
+            case "K" -> 13;
+            case "A" -> 14;
+            default -> Integer.parseInt(value);
+        };
+
+    }
 
 
 }
@@ -32,18 +52,23 @@ public class Card {
 
 class CardDeck {
 
-    static Scanner scanner = new Scanner(System.in);
-
 
     // A method to create a deck of 52 cards.
     public static void CreateDeck(ArrayList<Card> cards) {
 
         String[] validValues = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 
-        for (String value : validValues) {
-            for (Suit suit : Suit.values()) {
+        for (Suit suit : Suit.values()) {
+            for (String value : validValues) {
                 cards.add(new Card(value, suit));
             }
+        }
+
+        int rnd = new Random().nextInt(4);
+        for (int i = rnd * 13; i < rnd * 13 + 13; i++){
+
+            cards.get(i).trump = true;
+
         }
 
     }
@@ -63,36 +88,146 @@ class CardDeck {
     }
 
 
-    // A method to put a player's chosen card onto a deck.
-    public static void PlayFirstCard(ArrayList<Card> playerCards, LinkedHashMap<Card, Card> desk) {
+    // A method to check if a responder has a card to respond on a given card.
+    public static boolean CheckDeckForResponse(ArrayList<Card> cards, Card card) {
+
+
+        for (Card Response : cards) {
+
+            if (CheckCardForResponse(Response, card)) { return true; }
+
+        }
+
+        return false;
+
+    }
+
+
+    // A method to check if a card is able to respond on a given card.
+    public static boolean CheckCardForResponse(Card CardResponding, Card CardLying){
+
+        if (CardLying.trump){
+
+            if (CardResponding.trump && CardResponding.getValueInt() > CardLying.getValueInt()){
+
+                return true;
+
+            }
+
+        }
+
+        else {
+
+            if (CardResponding.trump) {
+
+                return true;
+
+            }
+
+            else {
+
+                if (CardLying.getSuit() == CardResponding.getSuit() && CardResponding.getValueInt() > CardLying.getValueInt()) {
+
+                    return true;
+
+                }}}
+
+
+        return false;
+
+    }
+
+
+    // Drawing a card from a deck of cards.
+    public static void DrawCard(ArrayList<Card> PlayerCards, ArrayList<Card> deck) {
+
+        if (deck.isEmpty()){return;}
+
+        int rnd = new Random().nextInt(deck.size());
+        PlayerCards.add(deck.get(rnd));
+        deck.remove(rnd);
+
+    }
+
+
+    // Method to make a responder take a card that he's been given to
+    public static void TakeGivenCard(ArrayList<Card> PlayerCards, LinkedHashMap<Card, Card> desk) {
+
+        PlayerCards.add(desk.lastEntry().getKey());
+        desk.remove(desk.lastEntry().getKey());
+
+    }
+
+
+    // A method to put a player's chosen card onto a desk.
+    public static void PlayFirstCard(ArrayList<Card> playerCards, ArrayList<Card> deck, LinkedHashMap<Card, Card> desk) {
+
+        Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < playerCards.size(); i++) System.out.println((i + 1) + ": " + playerCards.get(i).getCard());
         System.out.println("Pick a card: ");
 
-        int cardNum;
-        do {
+        int cardNum = scanner.nextInt();
+        while (cardNum < 1 || cardNum > playerCards.size()){
+
+            System.out.println("Please enter a number between 1 and " + playerCards.size());
             cardNum = scanner.nextInt();
 
-        } while (cardNum < 1 || cardNum > playerCards.size());
+
+        }
 
 
         desk.put(playerCards.get(cardNum - 1), null);
         playerCards.remove(cardNum - 1);
 
+        DrawCard(playerCards, deck);
+
+
     }
 
-    public static void PlaySecondCard(ArrayList<Card> playerCards, LinkedHashMap<Card, Card> desk) {
 
-        for (int i = 0; i < playerCards.size(); i++) System.out.println((i + 1) + ": " + playerCards.get(i).getCard());
-        System.out.println("Respond to a card " + desk.lastEntry().getKey() + ": ");
-
-        int cardNum;
-//        do {
-//            cardNum = scanner.nextInt();
-//
-//        } while (cardNum < 1 || cardNum > playerCards.size());
+    // A method to respond on a given card.
+    public static void Respond(ArrayList<Card> PlayerCards, LinkedHashMap<Card, Card> desk) {
 
 
+        if (CheckDeckForResponse(PlayerCards, desk.lastEntry().getKey()) == false){
+
+            System.out.println("No Response!!!");
+            TakeGivenCard(PlayerCards, desk);
+            return;
+
+        }
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < PlayerCards.size(); i++) System.out.println((i + 1) + ": " + PlayerCards.get(i).getCard());
+
+        System.out.println("Respond to a card " + desk.lastEntry().getKey().getCard() + ": ");
+        System.out.println("Or write 0 if you want to draw a card.");
+
+
+        int cardNum = scanner.nextInt();
+
+        if (cardNum == 0){
+            TakeGivenCard(PlayerCards, desk);
+            return;
+        }
+
+        while (!CheckCardForResponse(PlayerCards.get(cardNum - 1), desk.lastEntry().getKey())){
+
+            if (cardNum == 0){
+                TakeGivenCard(PlayerCards, desk);
+                return;
+            }
+
+            System.out.println("Write a valid card or write 0 if you want to draw a card.");
+            cardNum = scanner.nextInt();
+
+
+        }
+
+        desk.put(desk.lastEntry().getKey(), PlayerCards.get(cardNum - 1));
 
 
 
--- Main.java
+++ Main.java
@@ -7,48 +7,41 @@ public class Main {
     public static void main(String[] args) {
 
         // Create a deck of 52 cards
-        ArrayList<Card> cards = new ArrayList<>();
-        CardDeck.CreateDeck(cards);
+        ArrayList<Card> Deck = new ArrayList<>();
+        CardDeck.CreateDeck(Deck);
 
 
         // Create 2 players and give them 6 random cards from a deck
         // that we previously created.
         ArrayList<Card> player1 = new ArrayList<>();
-        CardDeck.SetRandomDeck6(player1, cards);
+        CardDeck.SetRandomDeck6(player1, Deck);
+
         ArrayList<Card> player2 = new ArrayList<>();
-        CardDeck.SetRandomDeck6(player2, cards);
+        CardDeck.SetRandomDeck6(player2, Deck);
 
 
         // Create a desk where the play is played.
-        // Key-Value function is essential in a card game.
         LinkedHashMap<Card, Card> desk = new LinkedHashMap<>();
 
 
-        System.out.println("Cards of a player 1: ");
-        for (int i = 0; i < player1.size(); i++) System.out.println((i + 1) + ": " + player1.get(i).getCard());
-
-        System.out.println();
-
-        System.out.println("Cards of a player 2: ");
-        for (int i = 0; i < player2.size(); i++) System.out.println((i + 1) + ": " + player2.get(i).getCard());
-        System.out.println();
+        PlayGame(Deck, player1, player2, desk);
 
 
     }
 
-    public static void PlayGame(ArrayList<Card> cards, ArrayList<Card> player1, ArrayList<Card> player2, ArrayList<Card> desk) {
+    public static void PlayGame(ArrayList<Card> cards, ArrayList<Card> player1, ArrayList<Card> player2, LinkedHashMap<Card, Card> desk) {
 
         Collections.shuffle(cards);
-        for (int i = 0; i < cards.size(); i++) System.out.println((i + 1) + ": " + cards.get(i).getCard());
 
 
-        while (!cards.isEmpty()) {
+        while (!player1.isEmpty() || !player2.isEmpty()) {
 
-
+            CardDeck.PlayFirstCard(player1, cards, desk);
+            CardDeck.Respond(player2, desk);
 
         }
 
 
     }
-}
 
+}
